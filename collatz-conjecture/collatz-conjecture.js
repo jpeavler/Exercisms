dc150573ab6d6bf44e73163ca5d9a108081d06d7
Joshua Peavler <joshuaapeavler@gmail.com>
2020-05-10 18:49:50 -0600

    completed Collatz exercism

---
+++ collatz-conjecture/collatz-conjecture.js
@@ -0,0 +1,20 @@
+//
+// This is only a SKELETON file for the 'Collatz Conjecture' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const steps = (n) => {
+  if(n < 1){
+    throw "Only positive numbers are allowed";
+  }
+  let count = 0;
+  while (n != 1){
+    if(n % 2 == 0){
+      n = n/2;
+    }else {
+      n = n*3 + 1;
+    }
+    count++;
+  }
+  return count;
+};
