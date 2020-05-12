572f4e8c0fec5eb50dc6e1b3a73f0d0b8a6a7a31
Joshua Peavler <joshuaapeavler@gmail.com>
2020-05-12 14:18:25 -0600

    Passed all tests

--- word-count/word-count.js
+++ word-count/word-count.js
@@ -4,10 +4,14 @@
 //
 
 export const countWords = (string) => {
-  let lettersAndNums = string.replace(/[#.:&@$%^!]/g, '');      //removes several non alphanumerics from input
+  let lettersAndNums = string.replace(/[#.:&@$%^!]/g, '');  //removes several non alphanumerics from input
   let noQuotes = lettersAndNums.replace(/\s'|'\s/g, ' ');   //removes single quotation marks without removing apostrophe
-  let stringArr = noQuotes.toLowerCase().split(/[\s\,]/); //splits input into an array
-  let removedBlanks = stringArr.filter(function (el) {  //removes all blank values in array
+  if(noQuotes.charAt((noQuotes.length - 1)) == "\'")        //removes single quotation if at end of string
+  {
+    noQuotes = noQuotes.slice(0, (noQuotes.length -1));
+  }
+  let stringArr = noQuotes.toLowerCase().split(/[\s\,]/);   //splits input into an array
+  let removedBlanks = stringArr.filter(function (el) {      //removes all blank values in array
     return el != "";
   });
   let countedWords = removedBlanks.reduce(function (allWords, word) { //creates object that counts freqency of
