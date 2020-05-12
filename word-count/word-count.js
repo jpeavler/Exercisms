b8c21e3e4e86ff090fe86affd7fe50d27848f329
Joshua Peavler <joshuaapeavler@gmail.com>
2020-05-12 12:01:59 -0600

    Passed 10 tests utilizing string.replace()

--- word-count/word-count.js
+++ word-count/word-count.js
@@ -4,7 +4,8 @@
 //
 
 export const countWords = (string) => {
-  let stringArr = string.toLowerCase().split(/[\s\,]/); //splits input into an array
+  let lettersAndNums = string.replace(/\W/g, ' ');
+  let stringArr = lettersAndNums.toLowerCase().split(/[\s\,]/); //splits input into an array
   let removedBlanks = stringArr.filter(function (el) {  //removes all blank values in array
     return el != "";
   });
