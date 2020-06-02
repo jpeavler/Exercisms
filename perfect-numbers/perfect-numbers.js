bea0ee39fbe2182f86779228b063839e3e37bcec
Joshua Peavler <joshuaapeavler@gmail.com>
2020-06-02 08:53:58 -0600

    Completed Perfect Numbers Exercism

--- perfect-numbers/perfect-numbers.js
+++ perfect-numbers/perfect-numbers.js
@@ -3,9 +3,26 @@
 // convenience to get you started writing code faster.
 //
 
-export const classify = (num) => {
-  if(num < 1) {
-    throw new Error("Classification is only possible for natural numbers.");
+const findFactors = (num) => {    //efficiently finds all factors of a number. O(sqrt(N)).
+  let factorList = [];
+  for (let i = 1; i*i < num; i++) {
+    if(num % i == 0) {
+      factorList.push(i);
+      factorList.push(num/i);
+    }
   }
-  
+  return factorList;
+}
+
+export const classify = (num) => {
+  if(num < 1) { throw new Error("Classification is only possible for natural numbers.") }
+  let sum = 0;
+  let factorList = findFactors(num);
+  factorList.forEach(x => sum = sum + x);
+  sum = sum - num;        //need to remove the number itself from the list
+  let result;
+  if(sum == num) { result = 'perfect'}
+  else if(sum > num) { result = 'abundant'}
+  else { result = 'deficient'}
+  return result;
 };
