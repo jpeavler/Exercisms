56b73999181fc8f7b61508f1e6c57caf19fb1ac4
Joshua Peavler <joshuaapeavler@gmail.com>
2020-06-04 10:15:55 -0600

    Added second regex to handle different cases of '-', all tests passed

--- acronym/acronym.js
+++ acronym/acronym.js
@@ -4,8 +4,9 @@
 //
 
 export const parse = (phrase) => {
-  let regex = /[_]/g;
-  let phraseArray = phrase.replace(regex, "").split(' ');
+  let regex = /_/g;
+  let regex2 = /-\b/g;
+  let phraseArray = phrase.replace(regex, "").replace(regex2, " ").split(' ');
   let acronym = "";
   phraseArray.forEach(word => {
     if(/[a-zA-Z]/.test(word[0])) {acronym = acronym + word[0];}
