08b6f6bc0ef165afd7f4ccb6310af4ed122bb0b0
Joshua Peavler <joshuaapeavler@gmail.com>
2020-06-04 11:50:41 -0600

    Downloaded Isogram exercism and passed all tests not involving repeated hyphens and spaces

---
+++ isogram/isogram.js
@@ -0,0 +1,18 @@
+//
+// This is only a SKELETON file for the 'Isogram' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const isIsogram = (phrase) => {
+  if(phrase == '') {return true}
+  let phraseArray = phrase.toLowerCase().split("");
+  let talliedPhrase = phraseArray.reduce((tally, letter) => {
+    tally[letter] = (tally[letter] || 0) + 1;
+    return tally;
+  }, {})
+  let countArray = Object.values(talliedPhrase);
+  for(let i = 0; i < countArray.length; i++){
+    if(countArray[i] > 1) {return false}
+  }
+  return true;
+};
