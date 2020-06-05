7d8c18b21e1c61df4389676546394e9d4010aef2
Joshua Peavler <joshuaapeavler@gmail.com>
2020-06-05 08:36:21 -0600

    Passed all tests of matching-brackets exercism

--- matching-brackets/matching-brackets.js
+++ matching-brackets/matching-brackets.js
@@ -7,19 +7,32 @@ export const isPaired = (string) => {
   let brackets = 0;
   let braces = 0;
   let para = 0;
+  let lastOpener = []
   for(let i = 0; i < string.length; i++) {
-    if(string[i] == "[") {brackets++;}
-    else if(string[i] == "{") {braces++;}
-    else if(string[i] == "(") {para++;}
+    if(string[i] == "[") {
+      brackets++;
+      lastOpener.push(string[i]);
+    }
+    else if(string[i] == "{") {
+      braces++;
+      lastOpener.push(string[i]);
+    }
+    else if(string[i] == "(") {
+      para++;
+      lastOpener.push(string[i]);
+    }
     else if(string[i] == "]") {
         brackets--;
         if(brackets < 0) {return false;}
+        if(lastOpener.pop() != "[") {return false;}
     } else if(string[i] == "}") {
         braces--;
         if(braces < 0) {return false;}
+        if(lastOpener.pop() != "{") {return false;}
     } else if(string[i] == ")") {
         para--;
         if(para < 0) {return false;}
+        if(lastOpener.pop() != "(") {return false;}
     }
   }
   if(brackets == 0 && braces == 0 && para == 0) {return true;}
