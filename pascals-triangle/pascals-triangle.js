0b005d42988041e0d0238787c7c9bba32f368ba9
Joshua Peavler <joshuaapeavler@gmail.com>
2020-06-22 11:41:14 -0600

    Downloaded Pascals Triangle Exercism in JavaScript

---
+++ pascals-triangle/pascals-triangle.js
@@ -0,0 +1,9 @@
+//
+// This is only a SKELETON file for the 'Pascals Triangle' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const rows = (rowCount) => {
+  let result = [];
+  return result;
+};
