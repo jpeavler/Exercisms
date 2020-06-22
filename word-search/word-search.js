9470d2947e6750c5b732e65db9003ab9d5433afc
Joshua Peavler <joshuaapeavler@gmail.com>
2020-06-22 14:48:03 -0600

    Downloaded Word Search Exercism for JavaScript

---
+++ word-search/word-search.js
@@ -0,0 +1,16 @@
+//
+// This is only a SKELETON file for the 'Word Search' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+class WordSearch {
+  constructor(grid) {
+    this._grid = grid;
+  }
+
+  find() {
+    throw new Error("Remove this statement and implement this function");
+  }
+}
+
+export default WordSearch;
