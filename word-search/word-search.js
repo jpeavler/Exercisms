b34b4ea7aa8fd641660107cdebf21c656458ae59
Joshua Peavler <joshuaapeavler@gmail.com>
2020-06-23 10:19:48 -0600

    Able to find words from top to bottom

--- word-search/word-search.js
+++ word-search/word-search.js
@@ -26,22 +26,30 @@ class WordSearch {
         for(let j = 0; j < this._grid[i].length; j++) {
           let reversedWord = reverseString(word);
           if(word[0] == this._grid[i][j]) {       //Checks for words left to right, starting with the first letter
-            let isHere = true;
+            let isHereRight = true;
             for(let k = 1; k < word.length; k++) {
-              if(word[k] != this._grid[i][j+k]) {isHere = false;} //if the next character isn't here, then the word isn't here
+              if(word[k] != this._grid[i][j+k]) {isHereRight = false;} //if the next character isn't here, then the word isn't here
             }
-            if(isHere){
+            if(isHereRight){
               coordinates = {"end": [i + 1, j + word.length], "start": [i + 1, j + 1]};
+            } else {                              //Checks for words top to bottom, starting with the first letter
+              let isHereDown = true;
+              for(let m = 1; m < word.length; m++) {
+                if(this._grid[i + m] == undefined) {isHereDown = false;}  //Prevents error of reading an index of undefined
+                else if(word[m] != this._grid[i+m][j]) {isHereDown = false;}
+              }
+              if(isHereDown) {
+                coordinates = {"end": [i + word.length, j + 1], "start": [i + 1, j + 1]};
+              }
             }
           } if(reversedWord[0] == this._grid[i][j]) { //Checks for words right to left, starting with the last letter
-            let isHereB = true;
+            let isHereLeft = true;
             for(let l = 1; l < word.length; l++) {
-              if(reversedWord[l] != this._grid[i][j+l]) {isHereB = false;}
+              if(reversedWord[l] != this._grid[i][j+l]) {isHereLeft = false;}
             }
-            if(isHereB) {
+            if(isHereLeft) {
               coordinates = {"end": [i + 1, j + 1], "start": [i + 1, j + word.length]}
             }
-            console.log("Will elixir coords change?", coordinates);
           }
         }
       }
