cf41f97b252d569c3e14cec4a0fc8fbe0d09d101
Joshua Peavler <joshuaapeavler@gmail.com>
2020-06-30 13:40:31 -0600

    Implemented mostly correct transpose function

--- transpose/transpose.js
+++ transpose/transpose.js
@@ -3,6 +3,24 @@
 // convenience to get you started writing code faster.
 //
 
-export const transpose = () => {
-  throw new Error("Remove this statement and implement this function");
+export const transpose = (arr) => {
+  let result = [];
+  let numCols = 0;
+  let numRows = 0;
+  arr.forEach(row => {
+    if(row.length > numCols) { numCols = row.length;}
+    numRows ++;
+  });
+  for(let i = 0; i < numCols; i++) {
+    let tempString = "";
+    for(let j = 0; j < numRows; j++) {
+      if(arr[j][i]) {
+        tempString = tempString.concat(arr[j][i]);
+      } else if(j + 1 != numRows){
+        tempString = tempString.concat(" ");
+      }
+    }
+    result.push(tempString);
+  }
+  return result;
 };
