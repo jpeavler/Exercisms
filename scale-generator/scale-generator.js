962154bd0f5b8928fdfa76af2054d2cf31835816
Joshua Peavler <joshuaapeavler@gmail.com>
2020-07-01 09:25:02 -0600

    Downloaded Scale Generator Exercism in JavaScript

---
+++ scale-generator/scale-generator.js
@@ -0,0 +1,13 @@
+export class Scale {
+  constructor(tonic) {
+    this._tonic = tonic;
+  }
+
+  chromatic() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  interval(intervals) {
+    throw new Error("Remove this statement and implement this function");
+  }
+}
