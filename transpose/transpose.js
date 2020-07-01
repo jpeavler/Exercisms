063be5dfeba8cb86fa536048bd47b9627d1c4d80
Joshua Peavler <joshuaapeavler@gmail.com>
2020-07-01 08:50:36 -0600

    Correctly removed right padding in last row, correcting the transpose function and completing the exercism

--- transpose/transpose.js
+++ transpose/transpose.js
@@ -1,26 +1,24 @@
-//
-// This is only a SKELETON file for the 'Transpose' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export const transpose = (arr) => {
   let result = [];
-  let numCols = 0;
-  let numRows = 0;
-  arr.forEach(row => {
+  let numCols = 0;  //Represents the number of columns the input has. Will become the number of Rows after Transpose
+  let numRows = 0;  //Represents the number of Rows the input has. Will become the number of Columns after Transpose
+  arr.forEach(row => {    //Allows me to find the longest row length (which will transpose to longest column)
     if(row.length > numCols) { numCols = row.length;}
-    numRows ++;
+    numRows ++;           //Tracks the number of Rows in the original array
   });
   for(let i = 0; i < numCols; i++) {
-    let tempString = "";
+    let tempString = "";  //a place to store each new row
     for(let j = 0; j < numRows; j++) {
       if(arr[j][i]) {
         tempString = tempString.concat(arr[j][i]);
-      } else if(j + 1 != numRows){
-        tempString = tempString.concat(" ");
+      } else{
+        tempString = tempString.concat(" ");  //creates left and right padding. The right padding will be removed on line 21.
       }
     }
     result.push(tempString);
+  }
+  if(result[numCols-1]) {                                    //This last bit trims off the right side padding in the last Row
+    result[numCols-1] = result[numCols-1].replace(/\s*$/,"");
   }
   return result;
 };
