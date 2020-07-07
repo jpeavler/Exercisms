1cbe3c1fe7b06311e164f2f74253079ba9c8fa61
Joshua Peavler <joshuaapeavler@gmail.com>
2020-07-07 11:32:06 -0600

    Completed Rotational Cipher Exercism in JavaScript

--- rotational-cipher/rotational-cipher.js
+++ rotational-cipher/rotational-cipher.js
@@ -1,5 +1,19 @@
+const alpha = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
 export class RotationalCipher {
-  static rotate() {
-    throw new Error("Remove this statement and implement this function");
+  static rotate(input, key) {
+    let result = "";
+    for(let i = 0; i < input.length; i++) {
+      if(input[i].match(/[A-Z]/g)) {
+        let oldIndex = alpha.indexOf(input[i].toLowerCase());
+        result = result.concat(alpha[(oldIndex + key) % 26].toUpperCase());
+      } else if(input[i].match(/[a-z]/g)) {
+        let oldIndex = alpha.indexOf(input[i]);
+        result = result.concat(alpha[(oldIndex + key) % 26])
+      } else {
+        result = result.concat(input[i]);
+      }
+    }
+    return result;
   }
 }
