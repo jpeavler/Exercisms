6a46822cfbba3a283c88b9c222df59b583044590
Joshua Peavler <joshuaapeavler@gmail.com>
2020-07-07 16:11:08 -0600

    Completed darts exercism

--- darts/darts.js
+++ darts/darts.js
@@ -1,3 +1,12 @@
-export const score = () => {
-  throw new Error("Remove this statement and implement this function");
+export const score = (x, y) => {
+  let dist = Math.sqrt(x**2 + y**2);
+  if(dist > 10) {
+    return 0;
+  } else if(dist > 5) {
+    return 1;
+  } else if(dist > 1) {
+    return 5;
+  } else {
+    return 10;
+  }
 };
