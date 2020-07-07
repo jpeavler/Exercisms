bfb3f64ab95cd4c26d92637405e4f9e2def3edfc
Joshua Peavler <joshuaapeavler@gmail.com>
2020-07-07 09:48:58 -0600

    Fixed condition check when invalid length is provided and translated Methionine properly

--- protein-translation/protein-translation.js
+++ protein-translation/protein-translation.js
@@ -1,8 +1,12 @@
 export const translate = (RNA) => {
   if(!RNA) {return [];}
-  if(!RNA.length % 3 == 0) {
+  if(!(RNA.length % 3 == 0)) {
     throw new Error('Invalid codon');
   }
   let result = [];
+  for(let i = 0; i < RNA.length; i= i+3) {
+    let currentCodon = RNA[i].concat(RNA[i+1], RNA[i+2]);
+    if(currentCodon == "AUG") {result.push('Methionine')}
+  }
   return result;
 };
