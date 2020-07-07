b1cc2f8f24a8c8ff3ea69f5bc92630acf74da9ce
Joshua Peavler <joshuaapeavler@gmail.com>
2020-07-07 10:00:22 -0600

    Implemented translation of all tested proteins

--- protein-translation/protein-translation.js
+++ protein-translation/protein-translation.js
@@ -6,7 +6,14 @@ export const translate = (RNA) => {
   let result = [];
   for(let i = 0; i < RNA.length; i= i+3) {
     let currentCodon = RNA[i].concat(RNA[i+1], RNA[i+2]);
-    if(currentCodon == "AUG") {result.push('Methionine')}
+    if(currentCodon == "AUG") {result.push("Methionine");}
+    else if(currentCodon == "UUU" || currentCodon == "UUC") {result.push("Phenylalanine");}
+    else if(currentCodon == "UUA" || currentCodon == "UUG") {result.push("Leucine");}
+    else if(currentCodon == "UCU" || currentCodon == "UCC"
+         || currentCodon == "UCA" || currentCodon == "UCG") {result.push("Serine");}
+    else if(currentCodon == "UAU" || currentCodon == "UAC") {result.push("Tyrosine");}
+    else if(currentCodon == "UGU" || currentCodon == "UGC") {result.push("Cysteine");}
+    else if(currentCodon == "UGG") {result.push("Tryptophan");}
   }
   return result;
 };
