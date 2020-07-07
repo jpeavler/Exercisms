395793b124a5dea58c58c8988b4941a6252c06f4
Joshua Peavler <joshuaapeavler@gmail.com>
2020-07-07 14:39:43 -0600

    Implemented encode function

--- run-length-encoding/run-length-encoding.js
+++ run-length-encoding/run-length-encoding.js
@@ -1,7 +1,25 @@
-export const encode = () => {
-  throw new Error("Remove this statement and implement this function");
+export const encode = (toEncode) => {
+  let encoded = "";
+  let tally = 1;
+  for(let i = 0; i < toEncode.length; i++) {
+    let lookAhead = i + 1;
+    let j = i;
+    while(toEncode[i] == toEncode[lookAhead]) {
+      tally ++;
+      lookAhead ++;
+      j ++;
+    }
+    if(tally > 1) {
+      encoded = encoded.concat(tally, toEncode[i]);
+    } else {
+      encoded = encoded.concat(toEncode[i]);
+    }
+    tally = 1;
+    i = j;
+  }
+  return encoded;
 };
 
-export const decode = () => {
+export const decode = (toDecode) => {
   throw new Error("Remove this statement and implement this function");
 };
