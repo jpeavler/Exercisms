401ca88a8ee93538babd64834963d1b760dc0254
Joshua Peavler <joshuaapeavler@gmail.com>
2020-07-07 11:57:13 -0600

    Completed armstrong numbers exercism

--- armstrong-numbers/armstrong-numbers.js
+++ armstrong-numbers/armstrong-numbers.js
@@ -1,8 +1,9 @@
-//
-// This is only a SKELETON file for the 'Armstrong Numbers' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-export const isArmstrongNumber = () => {
-  throw new Error("Remove this statement and implement this function");
+export const isArmstrongNumber = (num) => {
+  let str = num.toString();
+  let armSum = 0;
+  for(let i = 0; i < str.length; i++) {
+    armSum = armSum + (parseInt(str[i]))**str.length;
+  }
+  if(armSum == num) {return true;}
+  else {return false;}
 };
