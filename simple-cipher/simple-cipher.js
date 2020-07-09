1529dde7e38c6d9b3323c856eedd9258bf009e30
Joshua Peavler <joshuaapeavler@gmail.com>
2020-07-09 12:41:46 -0600

    installed simple cipher exercism

---
+++ simple-cipher/simple-cipher.js
@@ -0,0 +1,18 @@
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
+export class Cipher {
+  constructor() {
+    
+  }
+
+  encode(key) {
+    
+  }
+
+  decode() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  get key() {
+    throw new Error("Remove this statement and implement this function");
+  }
+}
