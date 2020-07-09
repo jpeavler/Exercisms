209334e685e292ebe67f3e02e829b752479dbfc0
Joshua Peavler <joshuaapeavler@gmail.com>
2020-07-09 12:05:01 -0600

    Completed Robot Exercism with inefficient solution

--- robot-name/robot-name.js
+++ robot-name/robot-name.js
@@ -1,6 +1,32 @@
-// This is only a SKELETON file for the 'Robot Name' exercise. It's been
-// provided as a convenience to get your started writing code faster.
+let generatedNames = [];
+const generateName = () => {
+    const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+    let letterOne = alphabet[Math.floor(Math.random() * 26)];
+    let letterTwo = alphabet[Math.floor(Math.random() * 26)];
+    let numOne = Math.floor(Math.random()* 10).toString();
+    let numTwo = Math.floor(Math.random()* 10).toString();
+    let numThree = Math.floor(Math.random()* 10).toString();
+    let name = letterOne.concat(letterTwo,numOne,numTwo,numThree);
+    if(generatedNames.includes(name)) {
+        name = generateName();
+    } else {
+        generatedNames.push(name);
+    }
+    return name;
+}
 
-export class Robot { }
+export class Robot {
+    constructor () {
+        this._name = generateName();
+    }
+    get name() {
+        return this._name;
+    }
+    reset() {
+        this._name = generateName();
+    }
+ }
 
-Robot.releaseNames = () => { };
+Robot.releaseNames = () => { 
+
+};
