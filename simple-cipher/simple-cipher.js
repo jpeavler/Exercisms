5ba4ff59e5b5f6a3948688bb836238ecc19c8084
Joshua Peavler <joshuaapeavler@gmail.com>
2020-07-10 17:12:21 -0600

    Completed simple cipher exercism

--- simple-cipher/simple-cipher.js
+++ simple-cipher/simple-cipher.js
@@ -1,18 +1,37 @@
-const alphabet = "abcdefghijklmnopqrstuvwxyz";
+const ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+const randomKey = () => {
+  let key = "";
+  for(let i = 0; i < 100; i++) {
+    key = key.concat(ALPHABET[Math.floor(Math.random() * 26)]);
+  }
+  return key;
+}
 export class Cipher {
-  constructor() {
-    
+  constructor(key) {
+    this._key = key ? key : randomKey();
   }
 
-  encode(key) {
-    
+  encode(str) {
+    let encoded = "";
+    for(let i = 0; i < str.length; i++) {
+      let incrementer = ALPHABET.indexOf(this._key[i% this._key.length]);
+      let originalIndex = ALPHABET.indexOf(str[i])
+      encoded = encoded.concat(ALPHABET[(incrementer + originalIndex) % 26]);
+    }
+    return encoded;
   }
 
-  decode() {
-    throw new Error("Remove this statement and implement this function");
+  decode(str) {
+    let decoded = ""
+    for(let i = 0; i < str.length; i++) {
+      let originalIndex = ALPHABET.indexOf(str[i]);
+      let decrementer = ALPHABET.indexOf(this._key[i % this._key.length])
+      decoded = decoded.concat(ALPHABET[(originalIndex - decrementer + 26) % 26]);
+    }
+    return decoded;
   }
 
   get key() {
-    throw new Error("Remove this statement and implement this function");
+    return this._key;
   }
 }
