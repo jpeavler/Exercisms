65de013ea08cdf9a693bb7b362aff66efb092e60
Joshua Peavler <joshuaapeavler@gmail.com>
2020-08-17 13:57:35 -0600

    Completed secret handshake exercism in JavaScript

--- secret-handshake/secret-handshake.js
+++ secret-handshake/secret-handshake.js
@@ -3,6 +3,26 @@
 // convenience to get you started writing code faster.
 //
 
-export const commands = () => {
-  throw new Error("Remove this statement and implement this function");
+export const commands = (num) => {
+  let binary = num.toString(2);
+  let result = [];
+  while(binary.length < 5) {
+    binary = "0" + binary;
+  }
+  if(binary[4] == "1") {
+    result.push("wink");
+  }
+  if(binary[3] == "1") {
+    result.push("double blink");
+  }
+  if(binary[2] == "1") {
+    result.push("close your eyes");
+  }
+  if(binary[1] == "1") {
+    result.push("jump");
+  }
+  if(binary[0] == "1") {
+    result = result.reverse()
+  }
+  return result;
 };
