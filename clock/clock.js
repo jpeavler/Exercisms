76fb620b7c32c18a6d6b9ec9d3a399fa51f2eb93
Joshua Peavler <joshuaapeavler@gmail.com>
2020-08-17 15:30:47 -0600

    Implemented constructor and toString function

--- clock/clock.js
+++ clock/clock.js
@@ -4,12 +4,41 @@
 //
 
 export class Clock {
-  constructor() {
-    throw new Error('Remove this statement and implement this function');
+  constructor(hour, min) {
+    this.hr = hour;
+    this.min = min;
+    while(this.min >= 60) {
+      this.min -= 60;
+      this.hr ++;
+    }
+    while(this.min < 0) {
+      this.min += 60;
+      this.hr --;
+    }
+    while(this.hr < 0) {
+      this.hr += 24;
+    }
+    this.hr = this.hr % 24;
   }
 
   toString() {
-    throw new Error('Remove this statement and implement this function');
+    let strHr;
+    let strMin;
+    if(!this.min) {
+      strMin = "00";
+    } else if (this.min.toString().length < 2) {
+      strMin = "0".concat(this.min);
+    } else {
+      strMin = this.min.toString();
+    }
+    if(this.hr < 10 && this.hr >= 0) {
+      strHr = "0" + this.hr.toString()
+    } else if (this.hr == 12 || this.hr == 24) {
+      strHr = "00";
+    } else {
+      strHr = this.hr.toString();
+    }
+    return strHr.concat(":", strMin);
   }
 
   plus() {
