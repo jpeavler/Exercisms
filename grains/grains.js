3eca12c26f55240bf4957fa1fc73264a3c9d87e7
Joshua Peavler <joshuaapeavler@gmail.com>
2020-08-24 11:56:50 -0600

    Implemented more efficient logic

--- grains/grains.js
+++ grains/grains.js
@@ -2,17 +2,9 @@ export const square = (numSquare) => {
   if(numSquare < 1 || numSquare > 64) {
     throw new Error("square must be between 1 and 64");
   }
-  let result = 1n;
-  for (let i = 2; i <= numSquare; i++) {
-    result = result * 2n;
-  }
-  return result;
+  return 2n ** BigInt(numSquare - 1);
 };
 
 export const total = () => {
-  let result = 0n;
-  for (let i = 1; i <= 64; i++) {
-    result = result + square(i);
-  }
-  return result;
+  return (2n ** 64n) - 1n;
 };
