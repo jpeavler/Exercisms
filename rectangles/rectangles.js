10d43350830dbfe4ac42182afc14a7c0041b7844
Joshua Peavler <joshuaapeavler@gmail.com>
2020-09-11 16:16:02 -0600

    Completed exercism

--- rectangles/rectangles.js
+++ rectangles/rectangles.js
@@ -1,10 +1,23 @@
-//
-// This is only a SKELETON file for the 'Rectangles' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
+//Was able to solve this exercism by heavily referencing thiagorasouza's code on exercim.io
 export class Rectangles {
-  static count() {
-    throw new Error("Remove this statement and implement this function");
+  static count(ascii) {
+    if(ascii.length === 0) return 0;
+    const scanRow = (startAt = 0) => {
+      const startRow = ascii[0];
+      const startCorner = startRow.indexOf('+', startAt);
+      if(startCorner === -1) return 0;
+      let count = 0;
+      for(let i = startCorner + 1; i < startRow.length; i++) {
+        if(startRow[i] !== "+") continue;
+        for(let j = 1; j < ascii.length; j++) {
+          const start = ascii[j][startCorner];
+          const end = ascii[j][i];
+          if (start === "+" && end == '+') count++;
+          if (!/[|+]{2}/.test(start + end)) break;
+        }
+      }
+      return count + scanRow(startCorner + 1);
+    }
+    return scanRow() + this.count(ascii.slice(1));
   }
 }
