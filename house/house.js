94d552713112ebe6da61d907a66142a424ad4243
Joshua Peavler <joshuaapeavler@gmail.com>
2020-09-15 17:03:56 -0600

    Completed house exercism

--- house/house.js
+++ house/house.js
@@ -1,9 +1,40 @@
+const opener = "This is the ";
+const lines = [
+  ["that lay in the ", "house that Jack built."],
+  ["that ate the ", "malt"],
+  ["that killed the ", "rat"],
+  ["that worried the ", "cat"],
+  ["that tossed the ", "dog"],
+  ["that milked the ", "cow with the crumpled horn"],
+  ["that kissed the ", "maiden all forlorn"],
+  ["that married the ", "man all tattered and torn"],
+  ["that woke the ", "priest all shaven and shorn"],
+  ["that kept the ", "rooster that crowed in the morn"],
+  ["that belonged to the ", "farmer sowing his corn"],
+  ["", "horse and the hound and the horn"]
+]
 export class House {
-  static verse() {
-    throw new Error("Remove this statement and implement this function");
+  static verse(ver) {
+    let result = [];
+    result.push(opener);
+    for(let i = ver; i >= 1; i--) {
+      if(i === ver) {
+        result[0] = result[0].concat(lines[i - 1][1]);
+      } else {
+        result.push(lines[i - 1][0].concat(lines[i - 1][1]));
+      }
+    }
+    return result;
   }
 
-  static verses() {
-    throw new Error("Remove this statement and implement this function");
+  static verses(start, end) {
+    let result = [];
+    for(let i = start; i <= end; i++) {
+      result = result.concat(this.verse(i));
+      if(i != end) {
+        result = result.concat([""]);
+      }
+    }
+    return result;
   }
 }
