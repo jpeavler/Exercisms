6be40ef7d7a2d8c6b6b5e22bd64c309a5a4093fe
Joshua Peavler <joshuaapeavler@gmail.com>
2020-09-15 15:08:01 -0600

    Installed House exercism

---
+++ house/house.js
@@ -0,0 +1,9 @@
+export class House {
+  static verse() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  static verses() {
+    throw new Error("Remove this statement and implement this function");
+  }
+}
