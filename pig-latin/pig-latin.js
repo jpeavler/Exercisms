a1f2b4cb66dd90d031cffa66d6d3fbcdf08a870f
Joshua Peavler <joshuaapeavler@gmail.com>
2020-10-26 17:04:25 -0600

    Passed most tests

--- pig-latin/pig-latin.js
+++ pig-latin/pig-latin.js
@@ -1,10 +1,32 @@
-//
-// This is only a SKELETON file for the 'Pig Latin' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
+const vowels = ["a", "e", "i", "o", "u"];
+const clusters = ["ch", "qu", "squ", "th", "thr", "sch", "rh"];
 export class translator {
-  static translate() {
-    throw new Error("Remove this statement and implement this function");
+  static translate(str) {
+    let result = str;
+    let startsVowel = false;
+    let startsConsCluster = -1;
+    vowels.forEach((letter, index) => {
+      if(str[0] == letter) {
+        startsVowel = true;
+      }      
+    });
+    clusters.forEach((cluster, index) => {
+      if(str[0] === cluster[0] && str[1] === cluster[1]) {
+        if(cluster[2] == undefined || cluster[2] === str[2]) {
+          startsConsCluster = index;
+        }
+      }
+    });
+    if(startsVowel) {
+      result = result + "ay";
+    } else if(startsConsCluster > -1) {
+      result = result.replace(clusters[startsConsCluster], "").concat(clusters[startsConsCluster], "ay");
+    } else {
+      let tempArr = result.split("");
+      let firstLetter = tempArr.shift();
+      tempArr.push(firstLetter);
+      result = tempArr.join("") + "ay";
+    }
+    return result;
   }
 }
