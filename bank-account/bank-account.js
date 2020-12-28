0ba02705fdcacbd52aa7af0cb119410b0b8970f8
Joshua Peavler <joshuaapeavler@gmail.com>
2020-12-28 15:57:53 -0700

    Completed bank account exercism in JavaScript.

--- bank-account/bank-account.js
+++ bank-account/bank-account.js
@@ -1,34 +1,59 @@
-//
-// This is only a SKELETON file for the 'Bank Account' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
+//Class BankAccount creates a new bank account with a balance: this._balance that starts at 0
+//and a boolean describing whether the account is open: this._is_open that starts as false.
 export class BankAccount {
   constructor() {
-    throw new Error('Remove this statement and implement this function');
+    this._balance = 0;
+    this._is_open = false;
   }
-
+//class method open changes this._is_open from false to true. If this._is_open is already true, it throws an error.
   open() {
-    throw new Error('Remove this statement and implement this function');
+    if(this._is_open) {
+      throw new ValueError("Account is already open.");
+    }
+    this._is_open = true;
   }
-
+//class method close changes this._is_open from true to false and resets balance to 0. If this._is_open is already false, it throws an error.
   close() {
-    throw new Error('Remove this statement and implement this function');
+    if(!this._is_open) {
+      throw new ValueError("Account is already closed.");
+    }
+    this._is_open = false;
+    this._balance = 0;
   }
-
-  deposit() {
-    throw new Error('Remove this statement and implement this function');
+//class method deposit adds parameter money to this._balance. Will throw an error when the account is closed (this._is_open === false) or
+//when the param money is a negative number.
+  deposit(money) {
+    if(!this._is_open) {
+      throw new ValueError("Account is closed.");
+    }
+    if(money < 0) {
+      throw new ValueError("Cannot deposit a negative amount of money.");
+    }
+    this._balance = this._balance + money;
   }
-
-  withdraw() {
-    throw new Error('Remove this statement and implement this function');
+//class method withdraw removes param money from this._balance. Will throw error when account is closed, money param is negative, or when
+//money param is greater than this._balance.
+  withdraw(money) {
+    if(!this._is_open) {
+      throw new ValueError("Account is closed.");
+    }
+    if(this._balance < money) {
+      throw new ValueError("Not enough funds to withdraw requested amount.");
+    }
+    if(money < 0) {
+      throw new ValueError("Cannot withdraw a negative amount of money.");
+    }
+    this._balance = this._balance - money;
   }
-
+//class get balance method returns the current balance of BankAccount. Will throw an error when the account is closed.
   get balance() {
-    throw new Error('Remove this statement and implement this function');
+    if(!this._is_open) {
+      throw new ValueError("Account is closed.");
+    }
+    return this._balance;
   }
 }
-
+//Class created for testing purposes by the writer of the exercism.
 export class ValueError extends Error {
   constructor() {
     super('Bank account error');
